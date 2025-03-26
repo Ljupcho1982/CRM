2119c7929ae6c81abb88d1e59c489001d57da695
Ljupcho1982 <ljupco.semov@gmail.com>
2025-03-26 20:11:23 +0100

    TirthCommit

---
+++ CRM.API/Areas/PublicArea/Controllers/PublicController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRM.API.Areas.PublicArea.Controllers
+
+
+{
+    [Area("PublicArea")]
+    [DisplayName("Public Controller")]
+    [Route("api/[area]/[controller]")]
+    [ApiController]
+    public class PublicController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            return Ok("Hello Public Area");
+        }
+    }
+}
